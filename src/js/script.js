33de721c1de2c6abbe712a2ea3d59b5f70984888
yasmin <labsfiap@fiap.com.br>
2026-05-05 11:51:03 -0300

    feat: style: palheta de cores com variaveis e estilização do card

--- src/js/script.js
+++ src/js/script.js
@@ -37,6 +37,47 @@ console.log(`R$ ${precoFinal.toFixed(2)}`); // `` (template string - contatenaç
 // DOM (document object model)
 const Titulo = document.getElementById("titulo")
 
-Titulo.innerText = "O DOM mudou o texto"
+Titulo.innerText = "O DOM mudou o texto";
 Titulo.style.color = "pink";
 
+// CRIANDO O APP
+const produtos = [
+    {nome: "Teclado Mecânico", preco: 300, emPromocao: true},
+    {nome: "Mouse Gamer", preco: 150, emPromocao: false},
+    {nome: "Monitor Gamer", preco: 1300, emPromocao: true},
+    {nome: "Pad Mouse XL", preco: 400, emPromocao: false},
+];
+
+const container =document.getElementById("listar-produtos");
+const mostrarTotal =document.getElementById("resultado-total");
+
+// Map: transforma um array de objetos em um array de strigs html em css
+
+function todosProdutos(lista){
+    mostrarTotal.innerText = "";
+    const htmlProdutos = lista.map(item =>`
+        <div class="card">
+            <h3>${item.nome}</h3>
+            <p>Preço: R$ ${item.preco}</p>
+            ${item.emPromocao ? '<span class="tag">Promoção</span>':''}
+        </div>
+        `).join(''); //transorma o array em uma unica string
+
+        container.innerHTML=htmlProdutos;
+}
+
+// FILTER: cria uma nova lista apenas com o que selecionou 
+
+function filtrarPromocoes(){
+    const promocionais = produtos.filter(item => item.emPromocao);
+    todosProdutos(promocionais);
+}
+
+// REDUCE: reduz o array a um unico valor (soma dos preços)
+
+function calcularTotal(){
+    const total = produtos.reduce((acumulador,item)=>{
+        return acumulador + item.preco;
+    },0);
+    mostrarTotal.innerText = `Valor Total R$ ${total}`;
+}
